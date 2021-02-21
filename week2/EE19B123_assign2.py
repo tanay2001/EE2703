341c8846029eadbf7ceeea7cef88f9c63518cca2
tanay2001 <dixittanay@gmail.com>
2021-02-21 21:07:13 +0530

    added some more functions

--- week2/EE19B123_assign2.py
+++ week2/EE19B123_assign2.py
@@ -3,7 +3,6 @@ EE2703 assignment 2
 Author: Tanay Dixit <ee19b123@smail.iitm.ac.in>
 Date Created: Feb 19, 2021
 """
-
 import numpy as np
 import sys
 
@@ -24,7 +23,7 @@ class Impedance():
     
 class Indsource():
     def __init__(self,line):
-            self.name, self.node1, self.node2, self.element = line[:4]
+            self.name, self.node1, self.node2, self.value = line[:4]
     def __getitem__(self,key):
         pass
 
@@ -66,18 +65,78 @@ def source2id(ls):
 
     return vMap
 
-def getTokens(line, nodes_list):
+def getCircuit(file, nodes_list):
     '''
     Finds all the elements attached to respective nodes
 
-    @args line: line of the netlist file (type list)
+    @args file: netlist file in list format (type list[list])
     @args nodes_list: set of unique nodes in the circuit (type set)
     returns a dict
     keys: nodes
     values : list of elements either having the node as "from" or "to" 
     '''
-    tokens = line.split()
-    l = len(tokens)
+    circuit ={}
+    for node in nodes_list:
+        circuit[node] =[]
+        for l in file:
+            #l : [name, n1,n2,value]
+            n1 = l[1]
+            n2 = l[2]
+            if n1 == node or n2 == node:
+                circuit[node].append(l[0])
+
+    return circuit
+
+def parseElements2obj(file):
+    '''
+    converts the elements to objects
+    @args : file netlist file in list format (type list[list])
+    returns : dict, key : elements, value: class instance
+    '''
+    element2obj ={}
+    for row in file:
+        #row =[element , n1,n2, value]
+        if row[0][0] in ['L','R','C']:
+            t = Impedance(row)
+
+        elif row[0][0] in ['V','I']:
+            t = Indsource(row)
+
+        element2obj[row[0]] =t
+    
+    return element2obj
+
+def solver(node2id, source2id, element2obj,circuit):
+    ''''
+    fills up the M  matrix in Mx = b
+    @args node2id: type(dict) node name-->id
+    @args source2id : type(dict) name--> id
+    @args element2obj : type(dict) element name-->class instance
+    @args circuit : type(dict) node --> elements connected to nodes
+    '''
+    M = np.empty((n+k, n+k))
+    y = np.empty((n+k,1))
+    ##fill M and y matrices and return x
+    ## Mx = y
+
+
+
+
+
+    return M
+
+if __name__ =='__main__':
+    #remove comments from netlist file
+    # create a cleam list[list] file 
+    #call app fucntions and solve circuit
+
+
+        
+
+
+
+
+
     
 
 

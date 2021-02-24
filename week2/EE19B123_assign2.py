6f6f5abab8c900b979ca77bb9cfcdeccaf60ad04
tanay2001 <dixittanay@gmail.com>
2021-02-24 13:33:05 +0530

    final done

--- week2/EE19B123_assign2.py
+++ week2/EE19B123_assign2.py
@@ -10,6 +10,7 @@ import numpy as np
 import sys
 import math
 import re
+import string
 
 class Impedance():
     ##class for setting up R,L,C instances
@@ -33,6 +34,7 @@ class Impedance():
         else:
             print("Please follow convention in naming elemenst in circuit")
             exit()
+
     def __getitem__(self,key):
         pass
     
@@ -41,13 +43,14 @@ class Indsource():
     def __init__(self,line, ac_flag):
             self.name, self.node1, self.node2= line[:3]
             if ac_flag:
-                mag = float(line[4])/2 ## cuz Vp-p is given
+                mag = float(line[4])/2 #### cuz Vp-p is given
                 self.phase = float(line[5])* (np.pi/180)
                 real = mag*np.cos(self.phase)
                 img = mag*np.sin(self.phase)
                 self.value = complex(real, img)
             else:
                 self.value = float(line[4])
+
     def __getitem__(self,key):
         pass
 
@@ -106,9 +109,9 @@ def getCircuit(file, nodes_list):
             #l : [name, n1,n2,value]
             n1 = l[1]
             n2 = l[2]
-            if n2 == node:
-                circuit[node]['from'].append(l[0])
             if n1 == node:
+                circuit[node]['from'].append(l[0])
+            if n2 == node:
                 circuit[node]['to'].append(l[0])
 
     return circuit
@@ -129,7 +132,10 @@ def parseElements2obj(file, flag,f):
             t = Impedance(l,flag,f)
 
         elif l[0][0] in ['V','I']:
-            t = Indsource(l, flag)
+            if 'ac' in l:
+                t = Indsource(l, True)
+            else:
+                t = Indsource(l, False)
 
         element2obj[l[0]] = t
 
@@ -163,53 +169,36 @@ def solver(node2id, source2id, element2obj,circuit, ac):
             for i in circuit[node]['from']:
                 #filling all from node values
                 if i[0] in ['R','L','C']:
-                    M[node2id[node],node2id[element2obj[i].node2]] += 1.0/element2obj[i].value
-                    M[node2id[node],node2id[element2obj[i].node1]] -= 1.0/element2obj[i].value
-                if ~ac:
-                    if i[0] =='V':
-                        M[node2id[node],source2id[i] + n -1] += 1
-                    elif i[0] =='I':
-                        y[node2id[node]] += (1)*element2obj[i].value
-                else:
-                    if i[0] =='V':
-                        M[node2id[node],source2id[i] + n -1] += 1
-                    elif i[0] =='I':
-                        y[node2id[node]] += (1)*element2obj[i].value
+                    M[node2id[node],node2id[element2obj[i].node1]] += 1.0/element2obj[i].value
+                    M[node2id[node],node2id[element2obj[i].node2]] -= 1.0/element2obj[i].value
+                if i[0] =='V':
+                    M[node2id[node],source2id[i] + n -1] += 1
+                elif i[0] =='I':
+                    y[node2id[node]] += (1)*element2obj[i].value
 
 
             for i in circuit[node]['to']:
                 #filling all to nodes values
                 if i[0] in ['R','L','C']:
-                    
-                    M[node2id[node],node2id[element2obj[i].node1]] += 1.0/element2obj[i].value
-                    M[node2id[node],node2id[element2obj[i].node2]] -= 1.0/element2obj[i].value
-                if ac:
-                    if i[0]=='V':
-                        M[node2id[node],source2id[i] + n -1] += -1
-                    elif i[0] =='I':
-                        y[node2id[node]] += (-1)*element2obj[i].value
-                else:
-                    if i[0]=='V':
-                        M[node2id[node],source2id[i] + n -1] += -1
-                    elif i[0] =='I':
-                        y[node2id[node]] += (-1)*element2obj[i].value
+                    M[node2id[node],node2id[element2obj[i].node2]] += 1.0/element2obj[i].value
+                    M[node2id[node],node2id[element2obj[i].node1]] -= 1.0/element2obj[i].value
+                if i[0]=='V':
+                    M[node2id[node],source2id[i] + n -1] += -1
+                elif i[0] =='I':
+                    y[node2id[node]] += (-1)*element2obj[i].value
 
 
     #populate source stuff
     for source in source2id:
-        M[n-1+ source2id[source], node2id[element2obj[source].node1]] = -1
-        M[n-1+ source2id[source], node2id[element2obj[source].node2]] = 1
-        if ~ac:
-            y[n -1 + source2id[source]] = element2obj[source].value
-        else:
-            y[n -1 + source2id[source]] = element2obj[source].value
+        M[n-1+ source2id[source], node2id[element2obj[source].node2]] = -1
+        M[n-1+ source2id[source], node2id[element2obj[source].node1]] = 1
+        y[n -1 + source2id[source]] = element2obj[source].value
+
 
     return M,y
 
 def comment_remover(lines):
     cir =[]
-    ##TODO
-    ### change CS case to dc/ac case print out saying currently cs course isn't supported
     for l in lines:
         l = re.sub('\n', '', l)
         ## remove comments from circuit
@@ -242,10 +231,12 @@ def cleaner(lines, flag, start, end, ac):
                 exit()
         elif tokens[0][0] in ['V','I']:
             try:
-                if flag:
-                    assert len(tokens) ==6 and flag, 'Inavlid format for sources follow Name n1 n2 ac value phase or Make sure .ac is present '
+                if flag and 'ac' in tokens:
+                    assert len(tokens) ==6 and flag, 'Inavlid format for sources follow Name n1 n2 ac value phase or \
+                        Make sure .ac is present '
                 else:
-                    assert len(tokens) ==5, 'Inavlid format for sources follow Name n1 n2 dc value or make sure ".ac" is present if using ac source'
+                    assert len(tokens) ==5, 'Inavlid format for sources follow Name n1 n2 dc value or \
+                        make sure ".ac" is present if using ac source'
             except AssertionError as msg:
                 print(msg)
                 exit()
@@ -253,7 +244,7 @@ def cleaner(lines, flag, start, end, ac):
             continue
 
         elif tokens[0] in ['E','G','H','F']:
-            print("sorry , controlled sources isn't supported as of such")
+            print("sorry , controlled sources isn't supported as of now")
             exit()
         else:
             print("invalid element please use convention")
@@ -273,7 +264,9 @@ if __name__ =='__main__':
         AC = '.ac'
         ac_map ={}
         ac_flag = False
+        freq = 0
         try:
+            print('-'*15+"Accuracy is upto 1e-8 so for all terms smaller its been rounded to 0"+'-'*15)
             with open(sys.argv[1]) as f:
                 lines = f.readlines()
                 c = 0
@@ -281,9 +274,12 @@ if __name__ =='__main__':
                 freq_stuff =[]
 
                 ##test structure of file before going to ahead
-                dummy = comment_remover(lines)
-                assert START in ' '.join(dummy), 'File missing .circuit'
-                assert END in ' '.join(dummy), 'File missing .end'
+                dummy = ' '.join(comment_remover(lines))
+                assert START in dummy, 'File missing .circuit'
+                assert END in dummy, 'File missing .end'
+                assert dummy.index(START) < dummy.index(END), 'follow convention of .circuit --- .end'
+                assert dummy.count('RL') ==1 , 'Please use 1 load Resistor ie: name one resistor as RL '
+    
 
                 for l in lines:
                     tokens = l.split()
@@ -303,6 +299,8 @@ if __name__ =='__main__':
                         freq = int(tokens[2])
                         ac_map[el] = freq
                         ac_flag =True
+            if ac_flag:
+                assert dummy.index(START) < dummy.index(END)< dummy.index(AC), 'follow convention of .circuit --- .end .ac'
             f.close()
             ### cleaned up netlist file 
             circuit = cleaner(contains, ac_flag, START, END, AC)
@@ -311,10 +309,9 @@ if __name__ =='__main__':
             for l in circuit:
                 tokens = l.split()
                 element, n1,n2 = tokens[:3]
-                if element[0] in ['R','L','C']:
-                    n_list.append(n1)
-                    n_list.append(n2)
-                elif element[0] in ['V']:
+                n_list.append(n1)
+                n_list.append(n2)
+                if element[0] in ['V']:
                     s_list.append(element)
 
             assert 'GND' in n_list, 'Missing GND node please add/replace one'
@@ -322,7 +319,7 @@ if __name__ =='__main__':
             node_set = np.unique(n_list)
             source_set = np.unique(s_list)
             ###
-        
+
             ### creating the maps 
             n2id = node2id(node_set)
             s2id = source2id(source_set)
@@ -337,17 +334,28 @@ if __name__ =='__main__':
             try:
                 x=np.linalg.solve(M, y)
             except:
-                print("matrix can't be solved")
+                print("Matrix can't be solved")
+                exit()
                 
             i =0
-            for k in n2id:
-                print("Voltage at {} is {} + j{}".format(k,x[i].real[0].astype(np.float16),\
+            for n in n2id:
+                print("Voltage at {} is {} + j{}".format(n,x[i].real[0].astype(np.float16),\
                     x[i].imag[0].astype(np.float16)))
                 i +=1
-            for v in s2id:
-                print("Current through {} is {} + j{}".format(v,x[i].real[0].astype(np.float16),\
+            for m in s2id:
+                print("Current through {} is {} + j{}".format(m,x[i].real[0].astype(np.float16),\
                     x[i].imag[0].astype(np.float16)))
                 i+=1
+            
+            ##TODO print current/
+            n1 =n2id[tokenObj['RL'].node1]
+            n2 = n2id[tokenObj['RL'].node2]
+            current = (x[n1] - x[n2])/tokenObj['RL'].value
+            voltage = x[n1]- x[n2]
+            print("current through RL is", current[0])
+            print('voltage through RL is',voltage[0])
+            # voltage across RL
+            print('-'*15+"only considering single frequency circuits as its mentioned so in the pdf"+'-'*15)
 
         except FileNotFoundError :
             print("File Not Found!!")
@@ -356,7 +364,7 @@ if __name__ =='__main__':
         print(msg)
         exit()
     
-    #call app fucntions and solve circuit
+
 
 
         

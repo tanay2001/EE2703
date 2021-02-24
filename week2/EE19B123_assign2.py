bca45241c2a5726c284b3ef57a260eba349aeec5
tanay2001 <dixittanay@gmail.com>
2021-02-24 09:56:49 +0530

    ac done

--- week2/EE19B123_assign2.py
+++ week2/EE19B123_assign2.py
@@ -1,13 +1,18 @@
 """
+
 EE2703 assignment 2
 Author: Tanay Dixit <ee19b123@smail.iitm.ac.in>
 Date Created: Feb 19, 2021
+
 """
+
 import numpy as np
 import sys
 import math
+import re
 
 class Impedance():
+    ##class for setting up R,L,C instances
     def __init__(self,line,ac_flag, freq=0):
         self.name, self.node1, self.node2 = line[:3]
         w = 2*np.pi*freq
@@ -24,7 +29,7 @@ class Impedance():
             if ac_flag:
                 self.value = complex(0,(w*L))
             else:
-                self.value = 0
+                self.value = 1e-10
         else:
             print("Please follow convention in naming elemenst in circuit")
             exit()
@@ -32,11 +37,15 @@ class Impedance():
         pass
     
 class Indsource():
+    #class for setting up source instances
     def __init__(self,line, ac_flag):
             self.name, self.node1, self.node2= line[:3]
             if ac_flag:
-                self.value = float(line[4])
+                mag = float(line[4])/2 ## cuz Vp-p is given
                 self.phase = float(line[5])* (np.pi/180)
+                real = mag*np.cos(self.phase)
+                img = mag*np.sin(self.phase)
+                self.value = complex(real, img)
             else:
                 self.value = float(line[4])
     def __getitem__(self,key):
@@ -58,7 +67,6 @@ def node2id(l):
         idMap[i] = c
         c+=1
     #unit test
-    assert idMap.keys() != None
 
     return idMap
 
@@ -105,10 +113,12 @@ def getCircuit(file, nodes_list):
 
     return circuit
 
-def parseElements2obj(file, flag):
+def parseElements2obj(file, flag,f):
     '''
     converts the elements to objects
     @args : file netlist file in list format (type list[list])
+    @args : flag ac flag True is ac sourcesa are used
+    @args : f , freq of operation
     returns : dict, key : elements, value: class instance
     '''
     element2obj ={}
@@ -116,7 +126,7 @@ def parseElements2obj(file, flag):
         l = row.split()
         #l =[element , n1,n2, value]
         if l[0][0] in ['L','R','C']:
-            t = Impedance(l,flag)
+            t = Impedance(l,flag,f)
 
         elif l[0][0] in ['V','I']:
             t = Indsource(l, flag)
@@ -140,8 +150,8 @@ def solver(node2id, source2id, element2obj,circuit, ac):
         M = np.zeros((n+k, n+k), dtype=complex)
         y = np.zeros((n+k,1), dtype= complex)
     else:
-        M = np.zeros((n+k, n+k))
-        y = np.zeros((n+k,1))
+        M = np.zeros((n+k, n+k), dtype= np.float32)
+        y = np.zeros((n+k,1), dtype = np.float32)
         
     ##fill M and y matrices and return x
     ## Mx = y
@@ -164,12 +174,13 @@ def solver(node2id, source2id, element2obj,circuit, ac):
                     if i[0] =='V':
                         M[node2id[node],source2id[i] + n -1] += 1
                     elif i[0] =='I':
-                        y[node2id[node]] += (1)*element2obj[i].value*(# e^jwt)
+                        y[node2id[node]] += (1)*element2obj[i].value
 
 
             for i in circuit[node]['to']:
                 #filling all to nodes values
                 if i[0] in ['R','L','C']:
+                    
                     M[node2id[node],node2id[element2obj[i].node1]] += 1.0/element2obj[i].value
                     M[node2id[node],node2id[element2obj[i].node2]] -= 1.0/element2obj[i].value
                 if ac:
@@ -178,10 +189,10 @@ def solver(node2id, source2id, element2obj,circuit, ac):
                     elif i[0] =='I':
                         y[node2id[node]] += (-1)*element2obj[i].value
                 else:
-                    elif i[0]=='V':
+                    if i[0]=='V':
                         M[node2id[node],source2id[i] + n -1] += -1
                     elif i[0] =='I':
-                        y[node2id[node]] += (-1)*element2obj[i].value*(#e^jwt)
+                        y[node2id[node]] += (-1)*element2obj[i].value
 
 
     #populate source stuff
@@ -191,69 +202,65 @@ def solver(node2id, source2id, element2obj,circuit, ac):
         if ~ac:
             y[n -1 + source2id[source]] = element2obj[source].value
         else:
-            y[n -1 + source2id[source]] = element2obj[source].value*(#e^jwt)
+            y[n -1 + source2id[source]] = element2obj[source].value
 
     return M,y
 
-def cleaner(lines):
-    '''
-    cleans up the raw netlist file ie: removes comments
-    and finds inconsistencies in th circuit definition
-
-    @args circuit type(list[list])
-    returns circuit 
-    '''
+def comment_remover(lines):
     cir =[]
     ##TODO
     ### change CS case to dc/ac case print out saying currently cs course isn't supported
     for l in lines:
-        tokens = l.split()
-        length = len(tokens)
-        if length==4 or (length>4 and tokens[4][0] =='#'):
-            ### l =4 implies impedance element
-            ### but incase a comment is present it may misleed the element type
-            ### hence total tokens apart from comments should be 4
-            element,n1,n2,value = tokens[:4]
-
-            ### confirming if all values are alphanumeric
-            try:
-                assert  n1.isalnum() and n2.isalnum(), "Node names need to be alphanumeric, please check Input file row"
-
-            except AssertionError as msg:  
-                print(msg)
-
-        elif length == 6 or (length>6 and tokens[6][0] =='#'):
-            ### l =6 implies VCVS/VCCS source
-            ### but incase a comment is present it may misleed the element type
-            ### hence total tokens apart from comments should be 6
-            element,n1,n2,n3,n4,value = tokens[:6]
-
-            ### confirming if all values are alphanumeric
-            try:
-                assert n1.isalnum() and n2.isalnum() and n3.isalnum() and n4.isalnum(), "Node names need to be alphanumeric, please check Input file row"
-            except AssertionError as msg:  
-                print(msg)
-
-        elif length==5 or (length>5 and tokens[5][0] =='#'):
-            ### l =5 implies CCVS/CCCS source
-            ### but incase a comment is present it may mislead the element type
-            ### hence total tokens apart from comments should be 5
-            element,n1,n2,V,value = tokens[:5]
-
-            ### confirming if all values are alphanumeric
-            try:
-                assert n1.isalnum() and n2.isalnum(),"Node names are alphanumeric, please check Input file row"
-            except AssertionError as msg:  
-                print(msg)
-
-
-
+        l = re.sub('\n', '', l)
         ## remove comments from circuit
         try:
             i = l.index('#')
         except:
             i = len(l)
-        cir.append(l[:i])
+        if l[:i]!='':
+            cir.append(l[:i])
+    return cir
+
+def cleaner(lines, flag, start, end, ac):
+    '''
+    cleans up the raw netlist file ie: removes comments
+    and finds inconsistencies in the circuit definition
+
+    @args circuit type(list[list])
+    returns circuit 
+    '''
+    ##remove comments
+    cir = comment_remover(lines)
+    ##now check if V,I is represented properly
+    for row in cir:
+        tokens = row.split()
+        if tokens[0][0] in ['R','L','C']:
+            try:
+                assert len(tokens) == 4, "Invalid format for impedances, follow Name n1 n2 value"
+            except AssertionError as msg:
+                print(msg)
+                exit()
+        elif tokens[0][0] in ['V','I']:
+            try:
+                if flag:
+                    assert len(tokens) ==6 and flag, 'Inavlid format for sources follow Name n1 n2 ac value phase or Make sure .ac is present '
+                else:
+                    assert len(tokens) ==5, 'Inavlid format for sources follow Name n1 n2 dc value or make sure ".ac" is present if using ac source'
+            except AssertionError as msg:
+                print(msg)
+                exit()
+        elif tokens[0] in [start, end, ac]:
+            continue
+
+        elif tokens[0] in ['E','G','H','F']:
+            print("sorry , controlled sources isn't supported as of such")
+            exit()
+        else:
+            print("invalid element please use convention")
+            exit()
+
+
+    ##now show CSS isn't supported currently
     return cir
             
         
@@ -264,12 +271,20 @@ if __name__ =='__main__':
         START = '.circuit'
         END = '.end'
         AC = '.ac'
+        ac_map ={}
         ac_flag = False
         try:
             with open(sys.argv[1]) as f:
                 lines = f.readlines()
                 c = 0
                 contains = []
+                freq_stuff =[]
+
+                ##test structure of file before going to ahead
+                dummy = comment_remover(lines)
+                assert START in ' '.join(dummy), 'File missing .circuit'
+                assert END in ' '.join(dummy), 'File missing .end'
+
                 for l in lines:
                     tokens = l.split()
                     if START == tokens[0] and (len(tokens)==1 or tokens[1][0] =='#'):
@@ -281,39 +296,44 @@ if __name__ =='__main__':
                             continue
                         assert len(l)>3,'The lines arent a valid circuit element'
                         contains.append(l)
-                    if AC==tokens[0] and (len(tokens)==1 or tokens[1][0] =='#'):
-                        freq = int(tokens[1])
+                    
+                    if AC==tokens[0]:
+                        assert len(tokens)==3,'Please folow format .ac V... freq'
+                        el = tokens[1]
+                        freq = int(tokens[2])
+                        ac_map[el] = freq
                         ac_flag =True
             f.close()
             ### cleaned up netlist file 
-            circuit = cleaner(contains)
-            
+            circuit = cleaner(contains, ac_flag, START, END, AC)
             ### create set of nodes and set of sources
             n_list =[]; s_list =[]
             for l in circuit:
                 tokens = l.split()
-                element, n1,n2,value = tokens[:4]
+                element, n1,n2 = tokens[:3]
                 if element[0] in ['R','L','C']:
                     n_list.append(n1)
                     n_list.append(n2)
                 elif element[0] in ['V']:
                     s_list.append(element)
 
+            assert 'GND' in n_list, 'Missing GND node please add/replace one'
             ### creating lists of unique nodes and sources
             node_set = np.unique(n_list)
             source_set = np.unique(s_list)
             ###
-
+        
             ### creating the maps 
             n2id = node2id(node_set)
             s2id = source2id(source_set)
 
+
             nodeMap = getCircuit(circuit, node_set,)
-            tokenObj = parseElements2obj(circuit, ac_flag)
+            tokenObj = parseElements2obj(circuit, ac_flag, freq)
 
             ## setting up M, y and solivng the eq'n
             M , y = solver(n2id, s2id, tokenObj,nodeMap, ac_flag)
-            print(M)
+            #print(M)
             try:
                 x=np.linalg.solve(M, y)
             except:
@@ -321,18 +341,20 @@ if __name__ =='__main__':
                 
             i =0
             for k in n2id:
-                print("Voltage at {} is {}".format(k,x[i]))
-                i = i+1
+                print("Voltage at {} is {} + j{}".format(k,x[i].real[0].astype(np.float16),\
+                    x[i].imag[0].astype(np.float16)))
+                i +=1
             for v in s2id:
-                print("Current through {} is {}".format(k,x[i]))
-
-            print(x)
+                print("Current through {} is {} + j{}".format(v,x[i].real[0].astype(np.float16),\
+                    x[i].imag[0].astype(np.float16)))
+                i+=1
 
         except FileNotFoundError :
             print("File Not Found!!")
 
     except AssertionError as msg:
         print(msg)
+        exit()
     
     #call app fucntions and solve circuit
 

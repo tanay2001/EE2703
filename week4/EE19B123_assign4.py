c02789d2061475fb311b69165fa128bb4a7b4001
tanay2001 <dixittanay@gmail.com>
2021-03-08 17:34:19 +0530

    assign4 ready

--- week4/EE19B123_assign4.py
+++ week4/EE19B123_assign4.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from scipy.integrate import quad 
 from scipy.linalg import lstsq
+import os
+if os.path.isdir('imgs'):
+    pass
+else:
+    os.mkdir('imgs')
+
 def exponential(x):
     return np.exp(x)
 
@@ -9,71 +15,32 @@ def coscos(x):
     return np.cos(np.cos(x))
 
 
-class wrapperplots():
-    def __init__(self, xrange = None ,label = None, yrange = None, path  = None,icon ='r-', plottype = 'plot', clear  = True, save = True):
-        super(wrapperplots, self).__init__()
-        self.Xrange = xrange
-        self.Yrange = yrange
-        self.label = label
-        self.img_path = path
-        self.plottype = plottype ## TODO add more types currently only plot, semilogy, semilogx,
-        self.saver = save
-        self.cleaner = clear
-        self.icon = icon
-        self.seteverything()
-
-    def plot(self):
-        #create plot object
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
-        self.ax.grid()
-
-    def seteverything(self):
-        self.plot()
-        if self.Xrange!=None:
-            self.ax.set_xlim(self.Xrange)
-        if self.Yrange!=None:
-            self.ax.set_ylim(self.Yrange)
-        if not self.label:
-            self.ax.legend()
-
-    def __call__(self, x, y):
-        assert len(x) ==len(y), "fucntions not same length"
-        if self.plottype =='plot':
-            self.ax.plot(x,y, self.icon)
-        elif self.plottype =='semilogy':
-            self.ax.semilogy(x, y, self.icon)
-        elif self.plottype =='semilogx':
-            self.ax.semilogx(x, y, self.icon)
-        elif self.plottype =='loglog':
-            self.ax.loglog(x, y, self.icon)
-        else:
-            print("not supported")
-        if self.saver:
-            self.ax.figure.savefig(self.img_path+'.png',bbox_inches='tight')
-            print("file saved at {}".format(self.img_path+'.png'))
-        if self.cleaner:
-            pass
-            #self.ax.clf()
-
-
-
-
-#TODO convert this to a wrapper class for multiple types of plots
-def plotdata(x,y, xrange = None ,label = None, yrange = None, path  = None, plottype = None, clear  = True):
-    assert len(x) ==len(y), "fucntions not same length"
-    if plottype =='semilogy': #TODO ad more plot types
-        plt.semilogy(x,y,'ro' ,label =label)
+def plotdata(x,y1,y2 =[] ,xname= None, yname = None ,icon = 'ro',label = 'plot', path  = None, plottype = None, clear  = True):
+    if len(y2) >0:
+        for i,y in enumerate([y1,y2]):
+            assert len(x) ==len(y), "fucntions not same length"
+            if plottype =='semilogy': #TODO ad more plot types
+                plt.semilogy(x,y,icon[i] ,label =label[i], ms =4)
+            elif plottype =='loglog':
+                plt.loglog(x,y,icon[i] ,label =label[i],ms =4) 
+            else:
+                plt.plot(x,y,'r-', label = label[i])
     else:
-        plt.plot(x,y, label = label)
+        assert len(x) ==len(y1), "fucntions not same length"
+        if plottype =='semilogy': #TODO ad more plot types
+            plt.semilogy(x,y1,icon ,label =label, ms =4)
+        elif plottype =='loglog':
+            plt.loglog(x,y1,icon ,label =label,ms =4) 
+        else:
+            plt.plot(x,y1,'r-', label = label)
+        
     plt.grid(True)
-    if not xrange:
-        plt.xlim(xrange)
-    if not yrange:
-        plt.ylim(yrange)
-    plt.legend()
-    plt.savefig(path+'.png',bbox_inches='tight')
-    print("file saved at {}".format(path+'.png'))
+    plt.legend(loc ='upper right')
+    plt.xlabel(xname)
+    plt.ylabel(yname)
+    if path !=None:
+        plt.savefig(path+'.png',bbox_inches='tight')
+        print("file saved at {}".format(path+'.png'))
     if clear:
         plt.clf()
 
@@ -101,51 +68,83 @@ def leastSquareCoef(func):
     return A, b
 
 
+
 if __name__ == "__main__":
+
     x = np.linspace(-2*np.pi,4*np.pi,300)
-    #true is x 
     #fourier will handle only (0,2pi) and make that periodic so plot this also
-    #plotdata(x, exponential(x), xrange=(-2*np.pi, 4*np.pi), path = 'exp_plot',label ='True function' ,plottype='semilogy', clear= False)
+    plotdata(x, exponential(x),xname= 'x', yname ='$exp^{x}$' , path = 'imgs/exp_plot',\
+        label ='True function' ,plottype='semilogy',icon='g-', clear= False)
 
     xnew =np.linspace(0,2*np.pi,100)
     ynew = exponential(xnew)
     #note its 3 peroids so 
     y = np.tile(ynew, 3)
-    #plotdata(x, y, xrange=(-2*np.pi, 4*np.pi), path = 'exp_plot',label= 'predicted fucntion', plottype='semilogy')
+    plotdata(x,y,xname ='x', yname='log (exp x)', path = 'imgs/exp_plot',\
+        label= 'predicted fucntion', plottype='semilogy', icon='r-')
 
+    plotdata(x, coscos(x), xname ='x', yname='cos(cos(x))', path = 'imgs/cos_plot',\
+        label= 'cos(cos(x)', plottype='plot', icon='r-')
 
-    #tr = wrapperplots(xrange=(-2*np.pi, 4*np.pi),label='cos(cos(x))', path = 'cos_plot2')
-    #tr(x, coscos(x))
-
-    #plotdata(x, coscos(x), xrange=(-2*np.pi, 4*np.pi),label='cos(cos(x))', path = 'cos_plot')
-
+    #############################################################################################################
     Fcoef_cos = fourier_coeff(51,coscos)
     Fcoef_exp = fourier_coeff(51,exponential)
 
-    #cos_ff = wrapperplots(icon= 'ro',plottype='semilogy', path='coeff_cos_semilog')
-    #cos_ff(range(1,52), np.abs(Fcoef_cos))
+    plotdata(x = range(1,52), y1 = np.abs(Fcoef_cos), xname = 'coeff', yname = 'log(value)', path = 'imgs/coef_cos_semilog',plottype='semilogy')
+    plotdata(x = range(1,52), y1 = np.abs(Fcoef_exp), xname = 'coeff', yname = 'log(value)', path = 'imgs/coef_exp_semilog',plottype='semilogy')
 
-
+    plotdata(x = range(1,52), y1 = np.abs(Fcoef_cos), xname = 'log(coeff)', yname = 'log(value)', path = 'imgs/coef_cos_log',plottype='loglog')
+    plotdata(x = range(1,52), y1 = np.abs(Fcoef_exp), xname = 'log(coeff)', yname = 'log(value)', path = 'imgs/coef_exp_log',plottype='loglog')
+    
     ##############################################################################################################
 
-    A, bexp = leastSquareCoef(exponential)
-    Lcoef_exp = lstsq(A,bexp)[0]
+    Aexp, bexp = leastSquareCoef(exponential)
+    Lcoef_exp = lstsq(Aexp,bexp)[0]
 
-    A, bcos = leastSquareCoef(coscos)
-    Lcoef_cos = lstsq(A,bcos)[0]
+    Acos, bcos = leastSquareCoef(coscos)
+    Lcoef_cos = lstsq(Acos,bcos)[0]
 
-    #cos_ff = wrapperplots(icon= 'ro',plottype='semilogy', path='lstq_cos')
-    #cos_ff(range(1,52), np.abs(Lcoef_cos))
+    plotdata(x = range(1,52), y1 = np.abs(Lcoef_cos),y2 = np.abs(Fcoef_cos),icon=['go','ro'], xname = 'coeff', yname = 'log(value)',\
+         label= ['lstq values', 'integration'], path = 'imgs/coef_cos_semilog',plottype='semilogy')
 
+    plotdata(x = range(1,52), y1 = np.abs(Lcoef_exp),y2 = np.abs(Fcoef_exp),icon=['go','ro'], xname = 'coeff', yname = 'log(value)',\
+         label= ['lstq values', 'integration'], path = 'imgs/coef_exp_semilog',plottype='semilogy')
 
-    #cos_ff = wrapperplots(icon= 'ro',plottype='semilogy', path='lstq_exp')
-    #cos_ff(range(1,52), np.abs(Lcoef_exp))
+    plotdata(x = range(1,52), y1 = np.abs(Lcoef_cos),y2 =np.abs(Fcoef_cos), icon=['go','ro'], xname = 'coeff', yname = 'log(value)',\
+         label= ['lstq values', 'integration'], path = 'imgs/coef_cos_log',plottype='loglog')
 
-    plt.semilogy(range(1,52), np.abs(Lcoef_exp), 'ro')
-    plt.savefig("example.png")
-
-
+    plotdata(x = range(1,52), y1 = np.abs(Lcoef_exp),y2 = np.abs(Fcoef_exp),icon=['go','ro'], xname = 'coeff', yname = 'log(value)',\
+         label= ['lstq values', 'integration'], path = 'imgs/coef_exp_log',plottype='loglog')
     ###########################################################################################################################
+
+
+    predicted_cos =  np.matmul(Acos,Lcoef_cos) 
+
+    predicted_exp  = np.matmul(Aexp, Lcoef_exp)
+
+    xcoord = np.linspace(0,2*np.pi, 401)[:-1]
+
+    plt.plot(xcoord, predicted_cos,'go',ms =4, label = 'predicted', )
+    plt.plot(xcoord, coscos(xcoord), label = 'True')
+    plt.grid(True)
+    plt.xlabel('x(linear)')
+    plt.ylabel('y(linear)')
+    plt.legend(loc = 'upper right')
+    plt.savefig('imgs/Figure1.png',bbox_inches='tight')
+    plt.clf()
+
+    plt.semilogy(xcoord, predicted_exp,'go',ms =4, label = 'predicted', )
+    ycoord = np.tile(exponential(xcoord), 3)
+    plt.semilogy(xcoord,exponential(xcoord), label = 'True')
+    plt.xlabel('x(linear)')
+    plt.ylabel('y(log)')
+    plt.grid(True)
+    plt.legend(loc = 'upper right')
+    plt.savefig('imgs/Figure0.png',bbox_inches='tight')
+    plt.clf()
+
+    #################################################################################################################################
+
 
 
 

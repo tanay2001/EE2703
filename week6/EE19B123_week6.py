374c5247dd4ea466245ffd4a5451c08b1b8e5741
tanay2001 <dixittanay@gmail.com>
2021-04-04 21:27:14 +0530

    done

--- week6/EE19B123_week6.py
+++ week6/EE19B123_week6.py
@@ -11,6 +11,8 @@ import argparse
 import mpl_toolkits.mplot3d.axes3d as p3
 import os
 import random
+import gc
+from tqdm import tqdm
 os.chdir('/home/tanay/Documents/sem4/EE2703/week6')#TODO remove this befor submitting
 
 #########################
@@ -44,15 +46,15 @@ class plot:
     def __init__(self):
         pass
     @save
-    def plot_intensity(self, I):
+    def plotIntensity(self, I, **kwargs):
         plt.hist(I,bins=np.arange(1,100),ec='black',alpha=0.5)
 
     @save
-    def plot_electron_density(self, X):
+    def plotEdensity(self, X, **kwargs):
         plt.hist(X, bins=np.arange(1,100),ec='black',alpha=0.5)
 
     @save
-    def plot_intensity_table(self, I):
+    def table(self, I, **kwargs):
         a,bins,c=plt.hist(I,bins=np.arange(1,100),ec='black',alpha=0.5)
         xpos=0.5*(bins[0:-1]+bins[1:])
         d={'Position':xpos,'Count':a}
@@ -60,24 +62,27 @@ class plot:
         print(p)
 
     @save
-    def plot_electron_phase_space(self, xx, u):
+    def plot_electron_phase_space(self, xx, u, **kwargs):
         plt.plot(xx,u,'x')
 
 
 
 class simulate(plot):
     '''
-
+    enviornment for Tubelight Simulation
     '''
     def __init__(self,dx,x,u):
         '''
+        @Params:
+        dx: current displacement
+        x : electron position
+        u : electron velocity
 
         '''
         super(simulate, self).__init__()
         self.dx  = dx # displacement at time i
         self.u  = u # e speed
         self.x = x # e postion
-        self.ids = np.where(self.x > 0)[0] #places where e is present
         self.I , self.V, self.X = [], [], []
 
 
@@ -88,6 +93,7 @@ class simulate(plot):
 
 
     def displace(self):
+        self.ids = np.where(self.x > 0)[0]
         self.dx[self.ids] = self.u[self.ids]  +0.5
         self.x[self.ids] += self.dx[self.ids]
         self.u[self.ids] += 1
@@ -98,31 +104,32 @@ class simulate(plot):
         
     def ionization(self,p, u0):
         velocity_ids = np.where(self.u >= u0)[0]
-        ll =  np.where(np.random.rand(len(velocity_ids))<=p)
+        ll =  np.where(np.random.rand(len(velocity_ids))<=p)[0]
         collision_ids = velocity_ids[ll]
         self.u[collision_ids] =0
 
         self.x[collision_ids] -= self.dx[collision_ids]*np.random.rand() #TODO improve algo
         # add photon
         self.I.extend(self.x[collision_ids].tolist())
+        #print('#Electrons Ionized')
 
     @staticmethod
     def injection(M, sigma):
         return np.random.randn()*sigma + M
 
     def inject(self,M, sigma):
-        num_of_e = self.injection(M, sigma)
+        num_of_e = int(self.injection(M, sigma))
         empty_slots = np.where(self.x==0)[0]
-        #TODO fixe error, peace out
-        numOfEadded = num_of_e if num_of_e < len(empty_slots) else empty_slots
+        numOfEadded = random.sample(empty_slots.tolist(),num_of_e) if num_of_e <= len(empty_slots) else empty_slots
 
         self.x[numOfEadded] = 1
         self.u[numOfEadded] =0
+        #print('##Electrons added')
 
     def run(self,args):
         M, sigma, p ,u0 ,n = args.M , args.Msigma, args.p , args.u0, args.n
         nk = args.nk
-        for _ in range(nk):
+        for _ in tqdm(range(nk)): #range(nk)
             #dispalce the electrons
             self.displace()
 
@@ -141,29 +148,32 @@ class simulate(plot):
             #identify elect properties
             self.findElectrons() #TODO optimise by ids + numOfEadded - hits(ll)
 
+        print(len(self.I), len(self.V), len(self.X))
         return self.I, self.V , self.X
 
             
-
-
 if __name__ =='__main__':
     parser = argparse.ArgumentParser()# use --help for support
-    parser.add_argument('--n',default=100,required = True, type=int,help='spatial grid size')
-    parser.add_argument('--M',default=5,required = True , type=int,help='number of electrons injected per turn')
-    parser.add_argument('--nk',default=500,required = True, type=int,help='number of turns to simulate')
-    parser.add_argument('--u0',default=5,required = True ,type=float,help='threshold velocity')
-    parser.add_argument('--p',default=0.25,required = True ,type=float,help='probability that ionization will occur')
-    parser.add_argument('--Msigma',default=0.2,required = True ,type=float,help='std in number of electrons added')
+    #TODO add required
+    parser.add_argument('--n',default=100, type=int,help='spatial grid size')
+    parser.add_argument('--M',default=5 , type=int,help='number of electrons injected per turn')
+    parser.add_argument('--nk',default=500, type=int,help='number of turns to simulate')
+    parser.add_argument('--u0',default=5,type=float,help='threshold velocity')
+    parser.add_argument('--p',default=0.25,type=float,help='probability that ionization will occur')
+    parser.add_argument('--Msigma',default=0.2,type=float,help='std in number of electrons added')
     args = parser.parse_args()
 
-    x , u, dx = np.zeros((1,args.n*args.M)), np.zeros((1,args.n*args.M)),np.zeros((1,args.n*args.M))
+    x , u, dx = np.zeros(args.n*args.M), np.zeros(args.n*args.M),np.zeros(args.n*args.M)
 
+    #creating the environment
     chamber = simulate(dx,x,u)
 
     I, V, X = chamber.run(args)
+    print('done running')
+    gc.collect()
 
-    chamber.plot_electron_density(X, path = 'electron_density')
-    chamber.plot_electron_phase_space(chamber.x , chamber.u, path ='phase+plot')
-    chamber.plot_intensity(I, path = 'plot_intensity')
-    chamber.plot_intensity_table(I, path = 'plot-table')
+    chamber.plotEdensity(X, path = 'imgs/electron_density')
+    chamber.plot_electron_phase_space(chamber.x , chamber.u, path ='imgs/phase+plot')
+    chamber.plotIntensity(I, path = 'imgs/plot_intensity')
+    chamber.table(I, path = 'imgs/plot-table')
 

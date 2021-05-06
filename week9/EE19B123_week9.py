1bdd38667dd63d1c78649d0fc89048827bdaa0f7
tanay2001 <dixittanay@gmail.com>
2021-05-06 12:13:07 +0530

    fixed a bug

--- week9/EE19B123_week9.py
+++ week9/EE19B123_week9.py
@@ -7,8 +7,9 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import os
-from numpy.fft import fftshift, fft
-os.chdir('/home/tanay/Documents/sem4/EE2703/week9')#TODO remove this befor submitting
+from pylab import *
+from numpy.fft import fftshift, fft, ifft
+#os.chdir('/home/tanay/Documents/sem4/EE2703/week9')#TODO remove this befor submitting
 #===================================================
 #creating an imgs directory to store all png files
 if os.path.isdir('imgs'):
@@ -35,6 +36,7 @@ def customplot(x,y,**kwargs):
     plt.subplot(2,1,1)
     plt.plot(x,np.abs(y),lw=2)
     plt.xlim([-kwargs['xlim'],kwargs['xlim']])
+    plt.xlabel('Frequency in rad/s')
     plt.ylabel(r"$|y|$", size = 12)
     plt.title(f"Spectrum of {kwargs['func']}")
     plt.grid(True)
@@ -42,75 +44,23 @@ def customplot(x,y,**kwargs):
     ii = np.where(np.abs(y)>kwargs['phase_limit'])
     plt.plot(x[ii], np.angle(y[ii]), 'ro', lw=2)
     plt.xlim([-kwargs['xlim'],kwargs['xlim']])
+    if kwargs.get('ylimit'):
+        plt.ylim([-kwargs['ylimit'],kwargs['ylimit']])
     plt.ylabel(r"Phase of $Y$", size=12)
-    plt.xlabel(r"$k$", size=12)
+    plt.xlabel('Frequency in rad/s', size = 12)
     plt.grid(True)
 
 if __name__ =='__main__':
-    #Examples 1 ========================================
-    x=np.linspace(0,2*np.pi,128)
-    y=np.sin(5*x)
-    Y=np.fft.fft(y)
+    #Example 0 ==========================================================
+    x=np.random.rand(100)
+    X=fft(x)
+    y=ifft(X)
+    c_[x,y]
+    maxError = max(np.abs(y-x))
+    print('Magnitude of maximum error between actual and computed values of the random sequence:', maxError)
 
-    plt.figure()
-    plt.subplot(2,1,1)
-    plt.plot(abs(Y),lw=2)
-    plt.grid(True)
-    plt.ylabel(r"$|Y|$")
-    plt.title(r"Spectrum of $\sin(5t)$")
-    plt.subplot(2,1,2)
-    plt.plot(np.unwrap(np.angle(Y)),lw=2)
-    plt.ylabel("Phase of Y")
-    plt.xlabel("k")
-    plt.grid(True)
 
-    #Examples 2 ========================================
-    x=np.linspace(0,2*np.pi,129)[:-1]
-    y=np.sin(5*x)
-    Y=np.fft.fftshift(np.fft.fft(y))/128.0
-    w=np.linspace(-64,63,128)
-    plt.figure()
-    plt.subplot(2,1,1)
-    plt.plot(w,abs(Y),lw=2)
-    plt.xlim([-10,10])
-    plt.ylabel(r"$|Y|$")
-    plt.title(r"Spectrum of $\sin(5t)$")
-    plt.grid(True)
-    plt.subplot(2,1,2)
-    ii=np.where(np.abs(Y)>1e-3)
-    plt.plot(w[ii],np.angle(Y[ii]),'go',lw=2)
-    plt.xlim([-10,10])
-    plt.ylabel(r"Phase of $Y$",size=16)
-    plt.xlabel(r"$k$",size=16)
-    plt.grid(True)
-
-    #Examples 3 ===========================================
-    t=np.linspace(0,2*np.pi,129);t=t[:-1]
-    y=(1+0.1*np.cos(t))*np.cos(10*t)
-    Y=np.fft.fftshift(np.fft.fft(y))/128.0
-    w=np.linspace(-64,63,128)
-
-    # Figure 1
-    plt.figure()
-    plt.subplot(2,1,1)
-    plt.plot(w,abs(Y),lw=2)
-    plt.xlim([-15,15])
-    plt.ylabel(r"$|Y|$",size=16)
-    plt.title(r"Spectrum of $\left(1+0.1\cos\left(t\right)\right)\cos\left(10t\right)$")
-    plt.grid(True)
-    
-    # Figure 2
-    plt.subplot(2,1,2)
-    #plt.plot(w,np.angle(Y),'ro',lw=2)
-    ii=np.where(np.abs(Y)>1e-3)
-    plt.plot(w[ii],np.angle(Y[ii]),'go',lw=2)
-    plt.xlim([-15,15])
-    plt.ylim([-3,3])
-    plt.ylabel(r"Phase of $Y$",size=16)
-    plt.xlabel(r"$\omega$",size=16)
-    plt.grid(True)
-
-    #Assignment Questions 
+    #Assignment Questions ==================================================
     def cos3(x):
         return np.cos(x)**3
 
@@ -123,12 +73,20 @@ if __name__ =='__main__':
     def coscos(x):
         return np.cos(20*x+5*np.cos(x))
 
-    func_dict = {'cos^3' : cos3,
+    def sin5(x):
+        return np.sin(5*x)
+
+    def modulated(x):
+        return (1+0.1*np.cos(x))*np.cos(10*x)
+
+    func_dict = {'sin':sin5,
+                'modul':modulated,
+                'cos^3' : cos3,
                 'sin^3' : sin3,
                 'coscos' : coscos,
                 'gauss' : gauss }
 
-    def dft(func,N=512,steps = 513, r=4*np.pi, phase_limit=1e-3, xlim=40, w_lim=64):
+    def dft(func,N=512,steps = 513, r=4*np.pi, phase_limit=1e-3, xlim=40, w_lim=64, ylimit=None):
         t = np.linspace(-r,r,steps)[:-1]
         y= func_dict[func](t)
         Y = fftshift(fft(y))/N
@@ -138,17 +96,22 @@ if __name__ =='__main__':
             # Normalizing 
             Y = Y*np.sqrt(2*np.pi)/np.max(Y)
             Y_ = np.exp(-w**2/2)*np.sqrt(2*np.pi)
-            print(f"max error is {np.abs(Y-Y_).max()}")
+            print(f"max error is {max(np.abs(Y-Y_))}")
 
         customplot(w,Y,
         func =func,
         path ='imgs/Q'+func,
         xlim = xlim ,
+        ylimit = ylimit,
         phase_limit = phase_limit)
         return Y,w
 
-    Y,w = dft('cos^3',xlim=15, steps =129, N = 128, w_lim=16)
-    Y,w = dft('sin^3',xlim=15)
-    Y,w = dft('coscos',xlim=40)
-    Y1,w =dft('gauss',N=512,r=8*np.pi,w_lim=32,xlim=10)
+    #sample Questions =========================================
+    Y1,w1= dft('sin', xlim=10)
+    Y2, w2 = dft('modul',xlim=40, ylimit = 1)
+    #assigments questions =====================================
+    Y3,w3 = dft('cos^3',xlim=15, steps= 129 , w_lim=16, N = 128, ylimit=1)
+    Y4,w4 = dft('sin^3',xlim=15)
+    Y5,w5 = dft('coscos',xlim=40)
+    Y6,w6 =dft('gauss',N=512,r=8*np.pi,w_lim=32,xlim=10)
 
